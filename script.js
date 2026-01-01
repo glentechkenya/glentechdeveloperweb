95c526bdaef69e731a449a5e2514d8825d8866e2
glentechkenya <glentech287@gmail.com>
2026-01-01 17:07:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Visitor & View Counters
+// Visitor & View Counters per browser
 let visitorCount = localStorage.getItem('visitors') || 0;
 let viewCount = localStorage.getItem('views') || 0;
 
@@ -10,3 +10,58 @@ localStorage.setItem('views', viewCount);
 
 document.getElementById('visitors').textContent = visitorCount;
 document.getElementById('views').textContent = viewCount;
+
+// Particle Background
+const canvas = document.getElementById('particles');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particlesArray = [];
+const colors = ['#0ff','#ff0','#f0f','#0f0'];
+
+class Particle {
+  constructor(){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random()*3+1;
+    this.speedX = Math.random()*1-0.5;
+    this.speedY = Math.random()*1-0.5;
+    this.color = colors[Math.floor(Math.random()*colors.length)];
+  }
+  update(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if(this.x<0 || this.x>canvas.width) this.speedX*=-1;
+    if(this.y<0 || this.y>canvas.height) this.speedY*=-1;
+  }
+  draw(){
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+    ctx.fill();
+  }
+}
+
+function init(){
+  for(let i=0;i<100;i++){
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  particlesArray.forEach(p=>{
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+window.addEventListener('resize', ()=>{
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
